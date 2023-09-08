42333c1e447d5a54c6ac5c5d5e9d6220f044d7d2
RosieOh <20172207@gm.hannam.ac.kr>
2023-09-08 10:09:51 +0900

    단위 테스트 실행

--- src/main/java/kr/co/teaspoon/dto/Sample.java
+++ src/main/java/kr/co/teaspoon/dto/Sample.java
@@ -1,12 +1,10 @@
 package kr.co.teaspoon.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class Sample {
