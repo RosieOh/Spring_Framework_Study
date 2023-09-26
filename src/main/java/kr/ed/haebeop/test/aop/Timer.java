9c9e02eaa5988dd9fdf54bc49730fc2275d2330e
RosieOh <20172207@gm.hannam.ac.kr>
2023-09-26 16:31:56 +0900

    실습 내용 기록

---
+++ src/main/java/kr/ed/haebeop/test/aop/Timer.java
@@ -0,0 +1,4 @@
+package kr.ed.haebeop.test.aop;
+
+public class Timer {
+}
---
+++ src/main/java/kr/ed/haebeop/test/proxy/BookServiceImpl.java
@@ -0,0 +1,13 @@
+package kr.ed.haebeop.test.proxy;
+
+import org.springframework.stereotype.Service;
+
+//구현체
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Override
+    public void rent(Book book) {
+        System.out.println("rent: " + book.getTitle());
+    }
+}
